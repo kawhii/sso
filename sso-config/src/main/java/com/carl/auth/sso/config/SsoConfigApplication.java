014035c2d0fec3aa63b657495fc021ec28f0d897
huangwb <huangwb@sunrizetech.com>
2017-09-11 10:50:13 +0800

    调整包名

--- sso-config/src/main/java/com/carl/auth/sso/config/SsoConfigApplication.java
+++ sso-config/src/main/java/com/carl/auth/sso/config/SsoConfigApplication.java
@@ -2,7 +2,7 @@
  * 版权所有.(c)2008-2017. 卡尔科技工作室
  */
 
-package com.carl.auth.ssoconfig;
+package com.carl.auth.sso.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
