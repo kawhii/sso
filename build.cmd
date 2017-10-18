595b24514a363c1cf225df730e1f65f7d9a04517
huangwb <huang.wenbin@foxmail.com>
2017-10-18 23:35:40 +0800

    完成核心初始化功能

--- build.cmd
+++ build.cmd
@@ -3,6 +3,7 @@
 
 @rem Check for mvn in path, use it if found, otherwise use maven wrapper
 @set MAVEN_CMD=mvn
+@set CURR_DIR=%cd%
 @where /q mvn
 
 
@@ -15,6 +16,7 @@
 @if "%1" == "help" call:help
 @if "%1" == "run-all" call:run-all
 @if "%1" == "hosts" call:hosts
+@if "%1" == "init" call:init
 
 @rem function section starts here
 @goto:eof
@@ -33,35 +35,51 @@
 
 :sso-server
 	@echo Stating sso server...
-	call cmd /k start "sso-server" %cd%/sso-server/build.cmd run
+	cd %CURR_DIR%/sso-server
+	start "sso-server" build.cmd run
+	cd %CURR_DIR%
 @goto:eof
 
 :sso-management
 	@echo Stating cas management...
-    call cmd /k start "sso-management" %cd%/sso-management/build.cmd run
+	cd %CURR_DIR%/sso-management
+    start "sso-management" build.cmd run
+    cd %CURR_DIR%
 @goto:eof
 
 :sso-config
 	@echo Stating config Server...
-	call cmd /k start "sso-config" %MAVEN_CMD% spring-boot:run -T 5 %1 %2 %3 -f sso-config/pom.xml
+	cd %CURR_DIR%
+	start "sso-config" %MAVEN_CMD% spring-boot:run -T 5 %1 %2 %3 -f sso-config/pom.xml
 @goto:eof
 
 :cas-client-demo
 	@echo Stating cas client demo...
-	call cmd /k start "cas-client-demo" %MAVEN_CMD% jetty:run -f sso-client-demo/sso-cas-client-demo/pom.xml
+	cd %CURR_DIR%
+	start "cas-client-demo" %MAVEN_CMD% jetty:run -f sso-client-demo/sso-cas-client-demo/pom.xml
 @goto:eof
 
 :shiro-client-demo
 	@echo Stating shiro client demo...
-	call cmd /k start "shiro-client-demo" %MAVEN_CMD% spring-boot:run -T 5 %1 %2 %3 -f sso-client-demo/sso-client-shiro-demo/pom.xml
+	cd %CURR_DIR%
+	start "shiro-client-demo" %MAVEN_CMD% spring-boot:run -T 5 %1 %2 %3 -f sso-client-demo/sso-client-shiro-demo/pom.xml
 @goto:eof
 
 :run-all
-	@echo TODO...
+	@echo Starting run all...
+	call:sso-config
+	call:sso-server
+	call:cas-client-demo
+	call:shiro-client-demo
+	call:sso-management
 @goto:eof
 
 :hosts
 	@echo start to set hosts...
 	@echo 127.0.0.1 passport.sso.co >>C:\WINDOWS\system32\drivers\etc\hosts
 	@echo success.
+@goto:eof
+
+:init
+	@echo init ssl and hosts...
 @goto:eof
