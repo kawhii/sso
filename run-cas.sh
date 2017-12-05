b3a2cbaaaa4a0e8b96117793905bfb4e21553e4a
huangwb <huangwb@sunrizetech.com>
2017-12-05 18:27:05 +0800

    配置中心尚未启动，导致启动失败，添加睡眠1分钟再启动cas

--- run-cas.sh
+++ run-cas.sh
@@ -2,4 +2,5 @@
 export JAVA_HOME=/opt/jre-home
 export PATH=$PATH:$JAVA_HOME/bin:.
 exec java -jar /usr/local/service/sso-config/target/sso-config.jar &
+sleep 1m
 exec java -jar /usr/local/service/sso-server/target/cas.war
