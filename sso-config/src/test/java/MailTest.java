568a899995263de0cf6fb294f480676b4c99de2e
huangwb <huangwb@sunrizetech.com>
2017-12-20 09:48:25 +0800

    添加邮件测试

---
+++ sso-config/src/test/java/MailTest.java
@@ -0,0 +1,41 @@
+/*
+ * 版权所有.(c)2008-2017. 卡尔科技工作室
+ */
+
+import com.carl.auth.sso.config.SsoConfigApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @author Carl
+ * @version 创建时间：2017/12/20
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SsoConfigApplication.class)
+public class MailTest {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String mail;
+
+
+    @Test
+    public void connect() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(mail);
+        message.setTo(mail); //自己给自己发送邮件
+        message.setSubject("主题：测试邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+    }
+}
